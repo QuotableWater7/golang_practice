7107f4725ea8fe9912e959f06d86bb29e95139d5
JT <jbowler2400@gmail.com>
2016-08-20 15:59:32 -0400

    Working quicksort in golang

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func partition(low int, high int, numbers []int) {
+  current_low := low
+
+  for i := low; i < high; i++ {
+    if numbers[i] < numbers[high] {
+      if i != current_low {
+        numbers[i], numbers[current_low] = numbers[current_low], numbers[i]
+      }
+
+      current_low++
+    }
+  }
+
+  numbers[current_low], numbers[high] = numbers[high], numbers[current_low]
+
+  if current_low - low > 1 {
+    partition(low, current_low - 1, numbers)
+  }
+
+  if high - current_low > 1 {
+    partition(current_low, high, numbers)
+  }
+
+}
+
+func quicksort(numbers []int) []int {
+  partition(0, len(numbers) - 1, numbers)
+
+  return numbers
+}
+
+func main() {
+  // var n int
+  // fmt.Scan(&n)
+
+  // var inputs = make([]int, n)
+  // for i := 0; i < n; i++ {
+  //   fmt.Scan(&inputs[i])
+  // }
+
+  var inputs = []int {2, 5, 3, 4, 10, 1, 7, 9, 8, 6, 15, 13, 14, 11, 12}
+
+  var output []int = quicksort(inputs)
+  for i := range output {
+    fmt.Printf("%d ", output[i])
+  }
+}
