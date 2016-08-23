cf43455c5ff790f66b496db81077e94541c0a9e5
JT <jbowler2400@gmail.com>
2016-08-23 09:54:27 -0700

    begin extracting a request struct

--- http/src/http/http.go
+++ http/src/http/http.go
@@ -5,20 +5,27 @@ import (
   "strings"
 )
 
+type Request struct {
+  RequestLine string
+  Headers [](*Header)
+}
+
 func Parse(packet string) {
+  request := Request{}
+
   lines := strings.Split(packet, "\n")
-  request_line := lines[0]
+  request.RequestLine = lines[0]
 
   fmt.Printf("\n%d lines in packet.\n", len(lines))
   fmt.Printf("*******************\n\n")
   fmt.Println("REQUEST LINE")
-  fmt.Println(request_line)
+  fmt.Println(request.RequestLine)
 
   fmt.Println()
   fmt.Println("******************\n")
 
-  headers := extractHeaders(lines)
-  for i := range headers {
-    fmt.Printf("%s: %s\n", headers[i].key, headers[i].value)
+  request.Headers = extractHeaders(lines)
+  for i := range request.Headers {
+    fmt.Printf("%s: %s\n", request.Headers[i].key, request.Headers[i].value)
   }
 }
